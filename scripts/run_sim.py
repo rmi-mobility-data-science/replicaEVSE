73a51ad4e5c29673d8a99c0e0aaae9a6d45254bd
mattcwilde <mattcwilde@gmail.com>
2023-04-29 22:10:35 +0000

    script working

--- scripts/run_sim.py
+++ scripts/run_sim.py
@@ -1,96 +1,125 @@
+"""This script runs the simulation for the Replica EVSE project. See the notebooks for details on how to get to this step."""
+
 import pandas as pd
-# import numpy as np
-from joblib import Parallel, delayed
-import replicaEVSE.sql_wrapper_functions as sql
-import replicaEVSE.load_curve_simulation_functions as sim
-
-datapath = '../../data/'
-
-
-with open('../src/metadata/MySQLpwd.txt') as f:
-    pw = f.readlines()
-pw = pw[0]
-connection_replica = sql.create_db_connection("localhost", "root", pw, 'replica')
-
-gdf = pd.read_pickle('../../data/blockgroup_boundaries.pkl')
-
-bgrp_list = list(gdf.GEOID.values)
+import numpy as np
+import dask.dataframe as dd
+import replicaEVSE.load_curve as sim
+import os
+import joblib
+import multiprocessing as mp
+from time import process_time
 
 
-def pull_person_list(connection, blockgroups):
-    """Pulls list of person_ids from SQL database given a list of destination blockgroups.
+# user defined variables
+# where is the data
+datadir = '../../data'
+simulation_id = 'base'
+mode = 'PRIVATE_AUTO'
 
-    Blockgroups are smaller than census tracts, so this is a more accurate representation of the population in the area.
-    
-    Parameters
-    ----------
-    connection : MySQL connector
-    blockgroups : list
-        List of destination blockgroups. The person_id attached to any trip ending in the blockgroup will be used as part of the simulation population
-        
-    Returns
-    -------
-    list
-        List of person_ids used in simulation
-    """
-    
-    q1 = """
-    SELECT person_id
-    FROM trips
-    WHERE destination_bgrp IN """+str(tuple(blockgroups))+""";
-    """
-    results_people = sql.read_query(connection, q1)
-    persons_df = sql.convert_to_df(results_people,['person_id'])
-    persons_df = persons_df.drop_duplicates()
-    
-    return list(set(persons_df.person_id))
-
-person_list = pull_person_list(connection_replica, bgrp_list)
-
-person_columns = [
-    'household_id', 'person_id', 'BLOCKGROUP', 'BLOCKGROUP_work', 'age',
-    'sex', 'race', 'ethnicity', 'individual_income', 'employment', 'education',
-    'industry', 'household_role', 'commute_mode', 'household_size', 'household_income',
-    'family_structure', 'vehicles', 'building_type', 'resident_type'
-]
-trips_columns = [
-    'activity_id', 'person_id', 'distance_miles', 'mode', 'travel_purpose', 'start_time',
-    'end_time', 'origin_bgrp', 'destination_bgrp', 'origin_land_use_l1', 'destination_land_use_l1',
-    'origin_building_use_l1', 'destination_building_use_l1', 'vehicle_type','weekday'
-]
+# number of chunks to break the dataframe into
+# this is used to parallelize the simulation
+# note: data set is ~50 million rows
+number_of_chunks = 10 # 10000 rows in each chunck
 
 #Created in the EIA_data_download.ipynb notebook
-existing_load=pd.read_csv(datapath+'EIA_demand_summary.csv')
+existing_load=pd.read_csv(os.path.join(datadir, 'EIA_demand_summary.csv'))
+
+test = False
 
 
-def create_lists(chunk_size, person_list):
-    """Creates list of person_id lists of a given chunk_size for parallelizing simulation
-    Parameters
-    ----------
-    chunk_size : int
-        Number of person ids to include in each sub-list
-        The simulation runs each sub-list of people on a single core in series
-    person_list : list
-        Person ids to reorganize into list of lists
-    Returns
-    -------
-    list
-        List of lists of person ids
-    """
-    lists = [person_list[x:x+chunk_size] for x in range(0, len(person_list), chunk_size)]
-    return lists
+# Start the stopwatch / counter 
+load_start_time = process_time() 
 
-#Divide list of person_ids into 1000-person sub-lists for parallelization
-chunked_list = create_lists(1000, person_list)
+# use dask?
+use_dask = False
+if use_dask:
 
-temp = Parallel(n_jobs=8)(
-    delayed(sim.run_charge_simulation)(
-        person_ids = i,
-        database_name = 'replica',
-        database_pw = pw,
-        person_columns = person_columns,
-        trips_columns = trips_columns,
-        existing_load = existing_load,
-        simulation_id = '01',
-        managed = False
-    ) for i in chunked_list)
+    from dask.distributed import Client, LocalCluster
+    from dask.diagnostics import ProgressBar
+
+    # start a local cluster and get the client address
+    cluster = LocalCluster(n_workers=int(0.9 * mp.cpu_count())) # Launches a scheduler and workers locally
+    client = Client(cluster)  # Connect to distributed cluster and override default
+    print(client)
+    pbar = ProgressBar()
+    pbar.register()
+
+
+    # load the joined data for trips that end in washington and people from the 
+    # population simulation. This uses dask but if you have a ton of memory you could try 
+    # loading it with pandas. 
+    ddf = dd.read_parquet(os.path.join(datadir, 'wa_pop_and_trips.parquet'))
+
+    # right now, only look at private auto trips
+    ddf = ddf.loc[ddf['mode'] == mode]
+
+    # sort on person_id and start_time
+    # ddf = ddf.sort_values(by=['person_id', 'start_time', 'weekday']).reset_index(drop=True)
+
+    # ddf = ddf.reset_index(drop=True)
+
+    # convert to pandas dataframe
+    # necessary for the next steps
+    print('---computing the dask data frame---')
+    df = ddf.compute()
+else:
+    if test:
+        df = pd.read_parquet(os.path.join(datadir, 'wa_pop_and_trips_subsample.parquet'))
+        df = df.loc[df['mode'] == mode]
+    else:
+        print('---reading the data frame---')
+        df = pd.read_parquet(os.path.join(datadir, 'wa_pop_and_trips.parquet'))
+        df = df.loc[df['mode'] == mode]
+
+# Stop the stopwatch / counter
+load_stop_time = process_time()
+
+print("time to load:", load_stop_time - load_start_time) 
+
+    # df = df.head(10000)
+
+chunck_start_time = process_time()
+# chunck_size = int(len(df)/number_of_chunks)
+print('---breaking the dataframe into chuncks---')
+# break the dataframe into chuncks
+# must be a df, not a ddf
+df_list = np.array_split(df, number_of_chunks)
+chunck_stop_time = process_time()
+print("time to break into chuncks:", chunck_stop_time - chunck_start_time)
+
+sim_start_time = process_time()
+# run the simulation in parallel
+# df must be a pandas dataframe
+charge_sims = joblib.Parallel(verbose=10, n_jobs=-1)(
+    joblib.delayed(sim.simulate_person_load)(
+    df=df,
+    existing_load=existing_load,
+    simulation_id='base',
+    managed=False
+) for df in df_list)
+sim_stop_time = process_time()
+print("time to simulate:", sim_stop_time - sim_start_time)
+
+# restack the dataframes
+# use dask to make use of the parallelization?
+print('---restacking the dataframes---')
+stack_start_time = process_time()
+charges_list = [x['charges'] for x in charge_sims]
+charges_df = pd.concat(charges_list)
+loads_list = [x['loads'] for x in charge_sims]
+loads_df = pd.concat(loads_list)
+stack_stop_time = process_time()
+print("time to stack:", stack_stop_time - stack_start_time)
+
+
+# save the results
+print('---saveing the charges and loads dfs---')
+save_start_time = process_time()
+charges_df.to_parquet(os.path.join(datadir, f'charges_{mode}_{simulation_id}.parquet'))
+loads_df.to_parquet(os.path.join(datadir, f'loads_{mode}_{simulation_id}.parquet'))
+save_stop_time = process_time()
+print("time to save:", save_stop_time - save_start_time)
+
+# close the client
+if use_dask:
+    client.close()
